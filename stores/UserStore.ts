69d927d13749952bbc2a902497d6dc9fa7328fdb
vikeedough <156529589+vikeedough@users.noreply.github.com>
2025-06-11 18:40:20 +0800

    Add Zustand and UserStore

---
+++ stores/UserStore.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+interface User {
+    id: number;
+    name: string;
+    avatar_url: string;
+}
+
+interface UserStore {
+    currentUser: User | null;
+    login: (user: User) => void;
+    logout: () => void;
+    isLoggedIn: boolean;
+}
+
+export const useUserStore = create<UserStore>()(
+    persist(
+        (set) => ({
+            currentUser: null,
+            login: (user: User) => set({ currentUser: user, isLoggedIn: true }),
+            logout: () => set({ currentUser: null, isLoggedIn: false }),
+            isLoggedIn: false,
+        }),
+        {
+            name: "user-storage",
+            storage: createJSONStorage(() => AsyncStorage),
+        }
+    )
+);
